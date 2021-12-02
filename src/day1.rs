feb2922f47050f31ec8528315968f1973a28a39c
Johnny T. Vestergaard <jtv@jrtv.dk>
2021-12-02 18:37:24 +0100

    refactor: prepare for more challenges

---
+++ src/day1.rs
@@ -0,0 +1,71 @@
+use anyhow::Result;
+use itertools::*;
+use serde::Deserialize;
+use std::path::Path;
+
+use crate::read_csv;
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct Day1Input {
+    depth: f64,
+}
+
+pub fn solve_puzzle(input_path: &Path) -> Result<()> {
+    let input = read_csv(input_path, &["depth"])?;
+    println!("Day1, Part1: {}", day1_part1(&input));
+    println!("Day1, Part2: {}", day1_part2(&input));
+    Ok(())
+}
+
+fn day1_part1(input: &[Day1Input]) -> usize {
+    input
+        .iter()
+        .tuple_windows()
+        .filter(|(a, b)| b.depth > a.depth)
+        .count()
+}
+
+fn day1_part2(input: &[Day1Input]) -> usize {
+    input
+        .iter()
+        .tuple_windows()
+        .map(|(a, b, c)| a.depth + b.depth + c.depth)
+        .tuple_windows()
+        .filter(|(a, b)| b > a)
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{create_input, read_csv};
+    use anyhow::Result;
+
+    #[test]
+    fn example_day1_part1() -> Result<()> {
+        let dir = tempfile::tempdir()?;
+        let path = create_input(
+            &dir,
+            &[
+                "199", "200", "208", "210", "200", "207", "240", "269", "260", "263",
+            ],
+        )?;
+        let input = read_csv(path.as_path(), &["depth"])?;
+        assert_eq!(7, day1_part1(&input));
+        Ok(())
+    }
+
+    #[test]
+    fn example_day1_part2() -> Result<()> {
+        let dir = tempfile::tempdir()?;
+        let path = create_input(
+            &dir,
+            &[
+                "199", "200", "208", "210", "200", "207", "240", "269", "260", "263",
+            ],
+        )?;
+        let input = read_csv(path.as_path(), &["depth"])?;
+        assert_eq!(5, day1_part2(&input));
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,130 @@
 use anyhow::{Context, Result};
 use csv::StringRecord;
-use itertools::*;
 use serde::Deserialize;
 use std::path::PathBuf;
 use std::{fs::File, path::Path};
 use structopt::StructOpt;
 
+mod day1;
+
 #[derive(Debug, StructOpt)]
 struct Args {
+    #[structopt(subcommand)]
+    puzzle: Day,
     input: PathBuf,
 }
 
-#[derive(Debug, Deserialize, PartialEq)]
-struct Input {
-    depth: f64,
+#[derive(Debug, StructOpt)]
+enum Day {
+    Day1,
+    Day2,
+    Day3,
+    Day4,
+    Day5,
+    Day6,
+    Day7,
+    Day8,
+    Day9,
+    Day10,
+    Day11,
+    Day12,
+    Day13,
+    Day14,
+    Day15,
+    Day16,
+    Day17,
+    Day18,
+    Day19,
+    Day20,
+    Day21,
+    Day22,
+    Day23,
+    Day24,
+    Day25,
 }
 
 fn main() -> Result<()> {
     let args = Args::from_args_safe()?;
-    let input = read_csv(args.input.as_path())?;
-
-    println!("Day1, Part1: {}", day1_part1(&input));
-    println!("Day1, Part2: {}", day1_part2(&input));
-
-    Ok(())
+    match args.puzzle {
+        Day::Day1 => day1::solve_puzzle(args.input.as_path()),
+        Day::Day2 => todo!(),
+        Day::Day3 => todo!(),
+        Day::Day4 => todo!(),
+        Day::Day5 => todo!(),
+        Day::Day6 => todo!(),
+        Day::Day7 => todo!(),
+        Day::Day8 => todo!(),
+        Day::Day9 => todo!(),
+        Day::Day10 => todo!(),
+        Day::Day11 => todo!(),
+        Day::Day12 => todo!(),
+        Day::Day13 => todo!(),
+        Day::Day14 => todo!(),
+        Day::Day15 => todo!(),
+        Day::Day16 => todo!(),
+        Day::Day17 => todo!(),
+        Day::Day18 => todo!(),
+        Day::Day19 => todo!(),
+        Day::Day20 => todo!(),
+        Day::Day21 => todo!(),
+        Day::Day22 => todo!(),
+        Day::Day23 => todo!(),
+        Day::Day24 => todo!(),
+        Day::Day25 => todo!(),
+    }
 }
 
-fn day1_part1(input: &[Input]) -> usize {
-    input
-        .iter()
-        .tuple_windows()
-        .filter(|(a, b)| b.depth > a.depth)
-        .count()
-}
-
-fn day1_part2(input: &[Input]) -> usize {
-    input
-        .iter()
-        .tuple_windows()
-        .map(|(a, b, c)| a.depth + b.depth + c.depth)
-        .tuple_windows()
-        .filter(|(a, b)| b > a)
-        .count()
-}
-
-fn read_csv(file_path: &Path) -> Result<Vec<Input>> {
+fn read_csv<T>(file_path: &Path, headers: &[&str]) -> Result<Vec<T>>
+where
+    for<'de> T: Deserialize<'de>,
+{
     let mut result = vec![];
     let reader = File::open(file_path)?;
-    let mut rdr = csv::Reader::from_reader(reader);
-    rdr.set_headers(StringRecord::from(vec!["depth"]));
+    let mut rdr = csv::ReaderBuilder::default()
+        .delimiter(b'\t')
+        .trim(csv::Trim::All)
+        .from_reader(reader);
+    rdr.set_headers(StringRecord::from(headers));
 
     for (line, record) in rdr.deserialize().enumerate() {
-        let record: Input = record.context(line)?;
+        let record: T = record.context(line)?;
         result.push(record);
     }
     Ok(result)
 }
 
 #[cfg(test)]
+fn create_input(dir: &tempfile::TempDir, lines: &[&str]) -> Result<PathBuf> {
+    let mut pb = PathBuf::from(dir.path());
+    pb.push("csvfile.csv");
+    let path = pb.as_path();
+    let mut file = File::create(path)?;
+    for l in lines {
+        use std::io::Write;
+        writeln!(file, "{}", l)?;
+    }
+    drop(file);
+    Ok(pb)
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
     use anyhow::Result;
-    use std::{fs::File, io::Write};
 
-    fn create_input(inp: &[&str]) -> Result<PathBuf> {
-        let mut pb = std::env::temp_dir();
-        pb.push("csvfile.csv");
-        let path = pb.as_path();
-        let mut file = File::create(path)?;
-        for l in inp {
-            writeln!(file, "{}", l)?;
-        }
-        drop(file);
-        Ok(pb)
+    #[derive(Debug, Deserialize, PartialEq)]
+    struct TestInput {
+        val: f64,
     }
 
     #[test]
     fn can_read_input() -> Result<()> {
-        let path = create_input(&["123", "456"])?;
+        let dir = tempfile::tempdir()?;
+        let path = create_input(&dir, &["123", "456"])?;
         assert_eq!(
-            vec![Input { depth: 123.0_f64 }, Input { depth: 456.0_f64 },],
-            read_csv(path.as_path())?
+            vec![TestInput { val: 123.0_f64 }, TestInput { val: 456.0_f64 },],
+            read_csv(path.as_path(), &["val"])?
         );
-        Ok(())
-    }
-
-    #[test]
-    fn example_day1_part1() -> Result<()> {
-        let path = create_input(&[
-            "199", "200", "208", "210", "200", "207", "240", "269", "260", "263",
-        ])?;
-        let input = read_csv(path.as_path())?;
-        assert_eq!(7, day1_part1(&input));
-        Ok(())
-    }
-
-    #[test]
-    fn example_day1_part2() -> Result<()> {
-        let path = create_input(&[
-            "199", "200", "208", "210", "200", "207", "240", "269", "260", "263",
-        ])?;
-        let input = read_csv(path.as_path())?;
-        assert_eq!(5, day1_part2(&input));
         Ok(())
     }
 }
