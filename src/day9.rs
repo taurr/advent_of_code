663ff4fb8630138cceffbf5cb98f0168f4ce892a
Johnny T. Vestergaard <jtv@jrtv.dk>
2021-12-09 21:59:02 +0100

    Day9 visualization

--- src/day9.rs
+++ src/day9.rs
@@ -1,48 +1,39 @@
 use anyhow::Result;
+use image::{GrayImage, ImageBuffer, Luma, Pixel, Rgb, RgbImage};
 use itertools::Itertools;
-use std::path::Path;
+use palette::{FromColor, Hsv, RgbHue, Srgb};
+use rand::Rng;
+use std::{ops::DerefMut, path::Path};
 
 pub fn solve_puzzle(input_path: &Path) -> Result<()> {
     let input = std::fs::read_to_string(input_path)?;
-    let input = parse_input(&input);
-    println!("Day9, Part1: {}", part1(&input));
-    println!("Day9, Part2: {}", part2(&input));
+    println!("Day9, Part1: {}", part1(parse_input(&input)?));
+    println!("Day9, Part2: {}", part2(parse_input(&input)?));
     Ok(())
 }
 
-fn parse_input(input: &str) -> Vec<Vec<u8>> {
-    input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|c| c.to_digit(10).unwrap() as u8)
-                .collect_vec()
-        })
-        .collect_vec()
+fn parse_input(input: &str) -> Result<GrayImage> {
+    let height = input.lines().count() as u32;
+    let container = input
+        .chars()
+        .filter_map(|c| c.to_digit(10))
+        .map(|d| d as u8)
+        .collect_vec();
+    GrayImage::from_raw(container.len() as u32 / height, height, container)
+        .ok_or(anyhow::anyhow!("Failed to create image"))
 }
 
-fn part1(input: &[Vec<u8>]) -> u32 {
-    let max_y = input.len() - 1;
-    let max_x = input[0].len() - 1;
-
-    let xy_window = { 0..=max_x }.flat_map(|x| { 0..=max_y }.map(move |y| (x, y)));
+fn part1(map: GrayImage) -> u32 {
+    let xy_window = { 0..map.width() }.flat_map(|x| { 0..map.height() }.map(move |y| (x, y)));
 
     let local_minima = xy_window.filter_map(|(x, y)| {
-        let local_minimum_x = { (x as i32 - 1).max(0) as usize..=(x + 1).min(max_x) }
-            .filter(|&lx| lx != x)
-            .map(|x| input[y][x])
-            .min()
-            .unwrap();
-        let local_minimum_y = { (y as i32 - 1).max(0) as usize..=(y + 1).min(max_y) }
-            .filter(|&ly| ly != y)
-            .map(|y| input[y][x])
-            .min()
-            .unwrap();
-        let local_minimum = local_minimum_x.min(local_minimum_y);
-
-        let local_value = input[y][x];
-        if local_value < local_minimum {
-            Some(local_value)
+        let v = map.get_pixel(x, y).0[0];
+        if !(x > 0 && v > map.get_pixel(x - 1, y).0[0]
+            || x < (map.width() - 1) && v > map.get_pixel(x + 1, y).0[0]
+            || y > 0 && v > map.get_pixel(x, y - 1).0[0]
+            || y < (map.height() - 1) && v > map.get_pixel(x, y + 1).0[0])
+        {
+            Some(v)
         } else {
             None
         }
@@ -51,66 +42,84 @@ fn part1(input: &[Vec<u8>]) -> u32 {
     local_minima.map(|m| 1 + m as u32).sum()
 }
 
-fn flood_fill<T: Copy + PartialOrd>(val: T, threshold: T, xy: (usize, usize), map: &mut [Vec<T>]) {
+fn part2(mut map: GrayImage) -> usize {
+    const THRESHOLD: u8 = 9;
+    let mut filler = Luma([THRESHOLD + 1]);
+    let mut sizes = vec![];
+    for y in 0..map.height() {
+        for x in 0..map.width() {
+            let pixels_filled = flood_fill(filler, |p| p.0[0] < THRESHOLD, (x, y), &mut map);
+            if pixels_filled > 0 {
+                filler = Luma([filler.0[0] + 1]);
+            }
+            sizes.push(pixels_filled);
+        }
+    }
+
+    let mut rgb = RgbImage::new(map.width(), map.height());
+    let mut rng = rand::thread_rng();
+    let colors = { THRESHOLD..filler.0[0] }
+        .map(|_| rng.gen_range(0f32..359f32))
+        .map(|hue| Hsv::new(RgbHue::from_degrees(hue), 1., 1.))
+        .map(Srgb::from_color)
+        .map(|srgb| {
+            let (r, g, b) = srgb.into_components();
+            Rgb([(r * 255.) as u8, (g * 255.) as u8, (b * 255.) as u8])
+        })
+        .collect_vec();
+    for y in 0..map.height() {
+        for x in 0..map.width() {
+            let g = map.get_pixel(x, y).0[0];
+            if g > THRESHOLD {
+                rgb.put_pixel(x, y, colors[(g - THRESHOLD - 1) as usize]);
+            }
+        }
+    }
+    rgb.save("day9_part2_viz.png").unwrap();
+
+    sizes.sort_unstable();
+    sizes.into_iter().rev().take(3).product()
+}
+
+fn flood_fill<F, P, Container>(
+    val: P,
+    func: F,
+    xy: (u32, u32),
+    map: &mut ImageBuffer<P, Container>,
+) -> usize
+where
+    P: Pixel + 'static,
+    Container: DerefMut<Target = [P::Subpixel]>,
+    F: Fn(&P) -> bool,
+{
+    let mut pixels: usize = 0;
     let mut q = vec![xy];
     while let Some((x, y)) = q.pop() {
-        if map[y][x] < threshold {
-            map[y][x] = val;
+        if func(map.get_pixel(x, y)) {
+            map.put_pixel(x, y, val);
+            pixels += 1;
             if x > 0 {
                 q.push((x - 1, y))
             }
-            if x < (map[0].len() - 1) {
+            if x < (map.width() - 1) {
                 q.push((x + 1, y))
             }
             if y > 0 {
                 q.push((x, y - 1))
             }
-            if y < (map.len() - 1) {
+            if y < (map.height() - 1) {
                 q.push((x, y + 1))
             }
         }
     }
-}
 
-fn part2(input: &[Vec<u8>]) -> u32 {
-    let max_y = input.len() - 1;
-    let max_x = input[0].len() - 1;
-    let mut bassins = input.iter().cloned().collect_vec();
-
-    const THRESHOLD: u8 = 9;
-
-    let mut last_counter = THRESHOLD + 1;
-    let mut inside = false;
-    let mut counter = 0;
-
-    for y in 0..=max_y {
-        for x in 0..=max_x {
-            if bassins[y][x] < THRESHOLD {
-                if !inside {
-                    inside = true;
-                    last_counter += 1;
-                    counter = last_counter;
-                };
-                flood_fill(counter, THRESHOLD, (x, y), &mut bassins);
-            } else {
-                inside = false;
-            }
-        }
-    }
-
-    let x = bassins
-        .into_iter()
-        .flat_map(|v| v.into_iter())
-        .filter(|&x| x > THRESHOLD)
-        .counts_by(|v| v);
-    let mut x = x.into_values().collect_vec();
-    x.sort_unstable();
-    x.into_iter().rev().take(3).product::<usize>() as u32
+    pixels
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use anyhow::Result;
 
     const INPUT: &str = indoc::indoc! {"
         2199943210
@@ -121,16 +130,18 @@ mod tests {
     "};
 
     #[test]
-    fn test_part1() {
+    fn test_part1() -> Result<()> {
         let input = parse_input(INPUT);
-        let result = part1(&input);
+        let result = part1(input?);
         assert_eq!(15, result);
+        Ok(())
     }
 
     #[test]
-    fn example_part2() {
+    fn example_part2() -> Result<()> {
         let input = parse_input(INPUT);
-        let result = part2(&input);
+        let result = part2(input?);
         assert_eq!(1134, result);
+        Ok(())
     }
 }
